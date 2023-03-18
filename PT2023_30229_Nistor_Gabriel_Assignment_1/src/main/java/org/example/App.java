4b8be345894d87513023b0bc96ea6ceb7ff8e16a
nistor1 <gnistor.gabriel1473@gmail.com>
2023-03-19 00:48:28 +0200

    initial commit

---
+++ PT2023_30229_Nistor_Gabriel_Assignment_1/src/main/java/org/example/App.java
@@ -0,0 +1,13 @@
+package org.example;
+
+//import org.example.DataModels.Monomial;
+//import org.example.DataModels.Polynomial;
+
+public class App {
+    public static void main( String[] args ) {
+        System.out.println( "Chiar merge!" );
+
+        new Monomial("-x");
+
+    }
+}
